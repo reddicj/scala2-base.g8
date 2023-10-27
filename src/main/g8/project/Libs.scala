4af98de5af30a225fad3ebe96c9d957aa580e2d6
James Reddick <reddicj@gmail.com>
2023-10-27 18:57:49 +0100

    Update

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -8,8 +8,8 @@ object Libs {
   val dependencies = libraryDependencies ++= Seq(
     "org.typelevel"              %% "cats-core"    % "2.10.0",
     "dev.zio"                    %% "zio"          % ZIO,
-    "dev.zio"                    %% "zio-test"     % ZIO % "test,it",
-    "dev.zio"                    %% "zio-test-sbt" % ZIO % "test,it",
+    "dev.zio"                    %% "zio-test"     % ZIO % "test",
+    "dev.zio"                    %% "zio-test-sbt" % ZIO % "test",
     "dev.zio"                    %% "zio-prelude"  % "1.0.0-RC21",
     "com.softwaremill.quicklens" %% "quicklens"    % "1.9.6"
   )
